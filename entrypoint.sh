6f1471d34be67c7d364e5b15e61aff0d8f118d23
baby9 <67196418+baby9@users.noreply.github.com>
2023-11-02 18:47:16 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-if [ ! -z $AUTH_USER ] && [ ! -z $AUTH_PASS ]; then
-    openvpn --up /etc/openvpn/update-resolv-conf --script-security 2 --config /ovpn.conf --auth-user-pass <(echo -e $AUTH_USER"\n"$AUTH_PASS) &
-else
-    openvpn --up /etc/openvpn/update-resolv-conf --script-security 2 --config /ovpn.conf &
-fi
+(
+    while [ -z $(cat /proc/net/dev | awk '{i++;if(i>2)print $1}' | sed 's/[:]*$//g' | grep 'tun0') ];
+    do
+        sleep 3
+    done
+    /usr/sbin/sockd -D
+) &
 
-sleep 10
-/usr/sbin/sockd
+if [ ! -z $AUTH_USER ] && [ ! -z $AUTH_PASS ]; then
+    openvpn --up /etc/openvpn/update-resolv-conf --script-security 2 --config /ovpn.conf --auth-user-pass <(echo -e $AUTH_USER"\n"$AUTH_PASS)
+else
+    openvpn --up /etc/openvpn/update-resolv-conf --script-security 2 --config /ovpn.conf
+fi
